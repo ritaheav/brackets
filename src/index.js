6f5cdf33faa5362cb4dbad00fedb1e02cc45361d
Ritaheav <rita20071995@hotmail.com>
2020-10-03 23:40:54 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+    for (let i = 0; i < str.length; i++) {
+        for(let j = 0; j < bracketsConfig.length; j++){
+            if (str[i] === bracketsConfig[j][1] && stack[stack.length - 1] === bracketsConfig[j][0]) {
+                stack.pop();
+            } else if (str[i] === bracketsConfig[j][0]) {
+                stack.push(str[i]);
+            }
+        }  
+    }
+    
+    return (stack.length === 0) ? true : false; 
 }
